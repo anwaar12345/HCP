e03d85014c91a850c8b74b7b027411f138db97a1
AnwaarAhmedshah <fa15be0011@maju.edu.pk>
2020-01-26 10:26:44 +0500

    Admin and user profiling hcp

--- hcp.sql
+++ hcp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2020 at 12:47 AM
+-- Generation Time: Jan 26, 2020 at 06:24 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `hcp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `docstatus`
+--
+
+CREATE TABLE `docstatus` (
+  `id` int(10) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `docstatus`
+--
+
+INSERT INTO `docstatus` (`id`, `status`) VALUES
+(1, 'pending'),
+(2, 'approved'),
+(3, 'blocked');
 
 -- --------------------------------------------------------
 
@@ -61,16 +81,22 @@ CREATE TABLE `tbldoctors` (
   `country` varchar(255) NOT NULL,
   `specialization` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `gender` varchar(10) NOT NULL
+  `gender` varchar(10) NOT NULL,
+  `status` int(10) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbldoctors`
 --
 
-INSERT INTO `tbldoctors` (`id`, `name`, `email`, `contact`, `address`, `regno`, `qualification`, `degimage`, `certimage`, `city`, `country`, `specialization`, `password`, `gender`) VALUES
-(1, 'Dr syed anwar ahmed  shah', 'syed123@gmail.com', '03012345654', 'karachi', '123443', 'MBBS', '72041755_2395208774072216_5816196432515825664_n.jpg', '81596780_585958348912912_8862971486982373376_n.jpg', 'karachi', 'pakistan', 'eye specialiat', '1630937c3d00b4f4b153599d93469963', 'male'),
-(2, 'Dr nee ww', 's@s', '4344556', 'fds', '123443', 'MBBS', '72041755_2395208774072216_5816196432515825664_n.jpg', '81596780_585958348912912_8862971486982373376_n.jpg', 'karachi', 'pakistan', 'eye specialiat', '1630937c3d00b4f4b153599d93469963', 'male');
+INSERT INTO `tbldoctors` (`id`, `name`, `email`, `contact`, `address`, `regno`, `qualification`, `degimage`, `certimage`, `city`, `country`, `specialization`, `password`, `gender`, `status`) VALUES
+(1, 'Dr syed anwar ahmed  shah', 'syed123@gmail.com', '03012345654', 'karachi', '123443', 'MBBS', '72041755_2395208774072216_5816196432515825664_n.jpg', '81596780_585958348912912_8862971486982373376_n.jpg', 'karachi', 'pakistan', 'eye specialiat', '1630937c3d00b4f4b153599d93469963', 'male', 2),
+(2, 'Dr nee ww', 's@s', '4344556', 'fds', '123443', 'MBBS', '72041755_2395208774072216_5816196432515825664_n.jpg', '81596780_585958348912912_8862971486982373376_n.jpg', 'karachi', 'pakistan', 'eye specialiat', '1630937c3d00b4f4b153599d93469963', 'male', 2),
+(3, 'dr test testing', 'drtst@email.com', '03012342654', 'dcfgrfgrvggvgrvfrvcfcfrcef', '123443', 'MBBS', '72041755_2395208774072216_5816196432515825664_n.jpg', '81596780_585958348912912_8862971486982373376_n.jpg', 'karachi', 'pakistan', 'heart specialiat', '1630937c3d00b4f4b153599d93469963', 'female', 2),
+(4, 'Dr stat us', 'us@us.com', '03012134321', 'new user doc', '123', 'mbbs', '82220525_471175433817246_8914716381086220288_n.jpg', '72041755_2395208774072216_5816196432515825664_n.jpg', 'khi', 'pak', 's', '1630937c3d00b4f4b153599d93469963', 'male', 2),
+(5, 'asxd fds', 'us@us.comw', '03012134321', 'dffdff', '123', 'mbbs', '81626197_845759099209945_1286228626525650944_n.jpg', '82220525_471175433817246_8914716381086220288_n.jpg', 'f', 'r', 'f', '108bc7b6961e71b2e770387a378cbc10', 'male', 2),
+(6, 'Dr statd fds', 'us@us.comss', '03012134321', 'sss', '123', 'mbbs', '81626197_845759099209945_1286228626525650944_n.jpg', '82220525_471175433817246_8914716381086220288_n.jpg', 'khi', 'pak', 's', '1630937c3d00b4f4b153599d93469963', 'male', 2),
+(7, 'Dr Danish Iqbal', 'drdanish@gmail.com', '03012134321', 'shahssdscdcds', '123', 'mbbs', '81626197_845759099209945_1286228626525650944_n.jpg', '82220525_471175433817246_8914716381086220288_n.jpg', 'khi', 'pak', 'eye ', '202cb962ac59075b964b07152d234b70', 'male', 2);
 
 -- --------------------------------------------------------
 
@@ -96,11 +122,19 @@ CREATE TABLE `tblusers` (
 INSERT INTO `tblusers` (`id`, `name`, `email`, `password`, `contact`, `image`, `gender`, `role`) VALUES
 (3, 'Syed Anwar Ahmed Shah', 'syedanwar016@gmail.com', 'cf43487cc21cd8ec0154bc3d4e5d3f23', 2147483647, '81626197_845759099209945_1286228626525650944_n.jpg', 'Male', 1),
 (4, 'test user', 'user@email.com', 'ee11cbb19052e40b07aac0ca060c23ee', 123, '81626197_845759099209945_1286228626525650944_n.jpg', 'Male', 2),
-(5, 'test user2', 'user2@email.com', '1630937c3d00b4f4b153599d93469963', 123, '82220525_471175433817246_8914716381086220288_n.jpg', 'Male', 2);
+(5, 'test user2', 'user2@email.com', '1630937c3d00b4f4b153599d93469963', 123, '82220525_471175433817246_8914716381086220288_n.jpg', 'Male', 2),
+(6, 'usertest1 test', 'ut@email.com', '1630937c3d00b4f4b153599d93469963', 2147483647, '81596780_585958348912912_8862971486982373376_n.jpg', 'Male', 2),
+(7, 'danish Iqbal', 'danish@gmail.com', '202cb962ac59075b964b07152d234b70', 2147483647, '72041755_2395208774072216_5816196432515825664_n (1).jpg', 'Male', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `docstatus`
+--
+ALTER TABLE `docstatus`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `roles`
@@ -127,6 +161,12 @@ ALTER TABLE `tblusers`
 --
 
 --
+-- AUTO_INCREMENT for table `docstatus`
+--
+ALTER TABLE `docstatus`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
@@ -136,13 +176,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `tbldoctors`
 --
 ALTER TABLE `tbldoctors`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
